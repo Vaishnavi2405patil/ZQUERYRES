e067dfcd95ff3023768cbb4c494af894e464c552
Vaishnavi Patil <vaishnavipatil24050@gmail.com>
2024-12-09 10:10:14 +0530

    First Commit

---
+++ webapp/controller/SApp.controller.js
@@ -0,0 +1,14 @@
+sap.ui.define(
+    [
+        "sap/ui/core/mvc/Controller"
+    ],
+    function(BaseController) {
+      "use strict";
+  
+      return BaseController.extend("zqueryres.controller.SApp", {
+        onInit: function() {
+        }
+      });
+    }
+  );
+  
