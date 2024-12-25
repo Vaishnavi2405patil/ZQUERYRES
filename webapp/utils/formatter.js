d5a49745079151df5cd96830300f8a16bbec0001
Vaishnavi Patil <vaishnavipatil24050@gmail.com>
2024-12-25 08:23:16 +0530

    25/12/24

---
+++ webapp/utils/formatter.js
@@ -0,0 +1,16 @@
+sap.ui.define([], function () {
+  "use strict";
+  var formatter = {
+    status: function (sStatus) {
+      if (sStatus === "Open") {
+        return "Error";
+      } else if (sStatus === "Closed") {
+        return "Success";
+      } else {
+        return none;
+      }
+    },
+  };
+
+  return formatter;
+});
